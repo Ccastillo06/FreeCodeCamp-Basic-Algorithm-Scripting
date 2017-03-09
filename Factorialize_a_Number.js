56ddeb80328e6fbc946bf946c010ebb7e66c690b
Cristian <ccastillon92@gmail.com>
2017-03-09 18:17:16 +0100

    Update and rename Factorialize a Number.js to Factorialize_a_Number.js

--- Factorialize_a_Number.js
+++ Factorialize_a_Number.js
@@ -14,8 +14,7 @@ function factorialize(num) {
 }
 
 
-factorialize(5) should return a number.
-factorialize(5) should return 120.
-factorialize(10) should return 3628800.
-factorialize(20) should return 2432902008176640000.
-factorialize(0) should return 1.
+factorialize(5) //should return 120.
+factorialize(10) //should return 3628800.
+factorialize(20) //should return 2432902008176640000.
+factorialize(0) //should return 1.
