673f404c60973c57e249cbfbf76d7c572d934e2d
Cristian <ccastillon92@gmail.com>
2017-03-10 14:24:54 +0100

    Update Reverse_a_String.js

--- Reverse_a_String.js
+++ Reverse_a_String.js
@@ -11,10 +11,7 @@ function reverseString(str) {
 }
 
 reverseString("hello world");
-
-
-
-reverseString("hello") //should return a string.
-reverseString("hello") //should become "olleh".
-reverseString("Howdy") //should become "ydwoH".
-reverseString("Greetings from Earth") //should return "htraE 
+reverseString("hello"); // should return a string.
+reverseString("hello"); // should become "olleh".
+reverseString("Howdy"); // should become "ydwoH".
+reverseString("Greetings from Earth"); // should return "htraE morf sgniteerG".
